f8651284e72f26d360a1d2c569e37e59551a58a9
Alexandr <ralex_01@mail.ru>
2021-06-20 16:27:53 +0300

    Creat one to many link - author of tasks

--- src/main/java/ru/webapp/controller/MainController.java
+++ src/main/java/ru/webapp/controller/MainController.java
@@ -1,11 +1,13 @@
 package ru.webapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.webapp.domain.Message;
+import ru.webapp.domain.User;
 import ru.webapp.repository.MessageRepository;
 
 import java.util.Map;
@@ -30,8 +32,12 @@ public class MainController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
 
         messageRepository.save(message);
 
