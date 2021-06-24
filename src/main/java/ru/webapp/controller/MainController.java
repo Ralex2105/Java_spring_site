208fb0d92687f60164a4ba49a7142af753132613
Alexandr <ralex_01@mail.ru>
2021-06-25 02:04:58 +0300

    add need function

--- src/main/java/ru/webapp/controller/MainController.java
+++ src/main/java/ru/webapp/controller/MainController.java
@@ -1,5 +1,6 @@
 package ru.webapp.controller;
 
+import net.bytebuddy.dynamic.loading.ByteArrayClassLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,7 +20,9 @@ import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller
@@ -107,7 +110,7 @@ public class MainController {
 
 
     @GetMapping("/main/{id}/download")
-    public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+    public void downloadResource(HttpServletRequest request, HttpServletResponse response,
                                     @PathVariable(value = "id") Long id) throws IOException {
 
         Message message = messageRepository.findById(id).orElseThrow();
@@ -118,14 +121,80 @@ public class MainController {
                 mimeType = "application/octet-stream";
             }
             response.setContentType(mimeType);
-
             response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
-
             response.setContentLength((int) file.length());
-
             InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-
             FileCopyUtils.copy(inputStream, response.getOutputStream());
         }
+    }
+
+    @GetMapping("/main/{id}/edit")
+    public String editById(@PathVariable(value = "id") long id, Model model) throws IOException {
+        if (!messageRepository.existsById(id)) {
+            return "main";
+        }
+        Message message = messageRepository.findById(id).orElseThrow();
+        ArrayList<Message> res = new ArrayList<>();
+        model.addAttribute("message", message.getId());
+        model.addAttribute("text", message.getText());
+        model.addAttribute("tag", message.getTag());
+        model.addAttribute("university", message.getUniversity());
+        model.addAttribute("type", message.getType());
+        model.addAttribute("nameOfCourse", message.getNameOfCourse());
+        model.addAttribute("yearOfStudy", message.getYearOfStudy());
+        model.addAttribute("filename", message.getFilename());
+
+        return "messageEdit";
+    }
+
+
+    @PostMapping("/main/{id}/edit")
+    public String edit(
+            @PathVariable(value = "id") long id,
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String university,
+            @RequestParam String yearOfStudy,
+            @RequestParam String type,
+            @RequestParam String nameOfCourse,
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        Message message = messageRepository.findById(id).orElseThrow();
+        message.setText(text);
+        message.setUniversity(university);
+        message.setCourse(yearOfStudy);
+        message.setType(type);
+        message.setNameOfCourse(nameOfCourse);
+        message.setTag(tag);
+
+        if(Files.exists(Paths.get(uploadPath + "/" + message.getFilename()))) {
+            Files.delete(Paths.get(uploadPath + "/" + message.getFilename()));
+        }
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            message.setFilenameForUser(file.getOriginalFilename());
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File("/" + uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+
+        model.put("messages", messages);
+
+        return "main";
     }
 }
